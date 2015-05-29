bca3bbe2cd23365efaf51dd86e6a49054baf6b65
beepscore <support@beepscore.com>
2015-05-28 17:47:48 -0700

    In SurveyTask.swift add nameQuestionStep.

--- Camelot/SurveyTask.swift
+++ Camelot/SurveyTask.swift
@@ -12,13 +12,18 @@ public var SurveyTask: ORKOrderedTask {
 
     var steps = [ORKStep]()
 
-    //NOTE: instructionStep can have an image
+    // NOTE: instructionStep can have an image
     let instructionStep = ORKInstructionStep(identifier: "IntroStep")
     instructionStep.title = "The Questions Three"
     instructionStep.text = "Who would cross the Bridge of Death must answer me these questions three, ere the other side they see."
     steps += [instructionStep]
 
-    //TODO: add name question
+    let nameAnswerFormat = ORKTextAnswerFormat(maximumLength: 20)
+    nameAnswerFormat.multipleLines = false
+    let nameQuestionStepTitle = "What is your name?"
+    let nameQuestionStep = ORKQuestionStep(identifier: "QuestionStep",
+        title: nameQuestionStepTitle, answer: nameAnswerFormat)
+    steps += [nameQuestionStep]
 
     //TODO: add 'what is your quest' question
 
