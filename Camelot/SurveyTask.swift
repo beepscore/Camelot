5d477d1c51eb251c3ecc0a96f3aa9ed29bbac649
beepscore <support@beepscore.com>
2015-05-28 17:11:41 -0700

    In SurveyTask.swift add code to return an ORKOrderedTask with identifier "SurveyTask"

--- Camelot/SurveyTask.swift
+++ Camelot/SurveyTask.swift
@@ -6,4 +6,21 @@
 //  Copyright (c) 2015 Beepscore LLC. All rights reserved.
 //
 
-import Foundation
+import ResearchKit
+
+public var SurveyTask: ORKOrderedTask {
+
+    var steps = [ORKStep]()
+
+    //TODO: add instructions step
+
+    //TODO: add name question
+
+    //TODO: add 'what is your quest' question
+
+    //TODO: add color question step
+
+    //TODO: add summary step
+
+    return ORKOrderedTask(identifier: "SurveyTask", steps: steps)
+}
